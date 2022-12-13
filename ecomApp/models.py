4cf4d4febad2ed0f3380322731b7df5628c7a608
ReshmaMMathews <reshmammathews2@gmail.com>
2022-12-13 15:37:56 +0530

    uploaded images

--- ecomApp/models.py
+++ ecomApp/models.py
@@ -44,7 +44,7 @@ class OrderItem(models.Model):
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
-    address = models.CharField(max_length=200, null=False)
+    address = models.CharField(max_length=250, null=False)
     city = models.CharField(max_length=200, null=False)
     state = models.CharField(max_length=200, null=False)
     zipcode = models.CharField(max_length=200, null=False)
