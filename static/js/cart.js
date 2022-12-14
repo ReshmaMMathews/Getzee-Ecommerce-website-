358c235d47fb58b1a2e9cad1885a9494bb126501
ReshmaMMathews <reshmammathews2@gmail.com>
2022-12-15 00:07:38 +0530

    csrf token in javascript

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,6 +1,6 @@
 var updateBtns = document.getElementsByClassName('update-cart')
 
-for (i=0; i<updateBtns.length; i++){
+for (var i=0; i<updateBtns.length; i++){
     updateBtns[i].addEventListener('click', function(){
         var productId = this.dataset.product
         var action = this.dataset.action
@@ -22,13 +22,15 @@ for (i=0; i<updateBtns.length; i++){
 function updateUserOrder(productId, action){
     console.log('User is authenticated, sending data.....')
 
-    var url = '/update_item/'
+    var url = '/update_item'
     
     /* create and use the fetch api to send a POST request */
+    
     fetch(url, {
         method: 'POST',
         headers: {
-            'Content-Type':'application/json'
+            'Content-Type':'application/json',
+            'X-CSRFToken':csrftoken,  /* after passing the csrf token inside main.html, send it */
         },
 
         /* converting(stringify) and sending our productId and action as a json object */
@@ -40,7 +42,7 @@ function updateUserOrder(productId, action){
     })
 
     .then((data) => {
-        console.log('Data:', data)
+        console.log('data:', data)
     });
 }
 
