b0644f0d8d8f9e9a2b11595f5f8729ce1eee2b54
ReshmaMMathews <reshmammathews2@gmail.com>
2022-12-20 19:36:50 +0530

    creating cookieCart function to clean views

--- ecomApp/views.py
+++ ecomApp/views.py
@@ -4,6 +4,8 @@ from . models import *
 import json
 import datetime
 
+from . utils import cookieCart
+
 
 
 
@@ -17,9 +19,8 @@ def store(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
-        cartItems = order['get_cart_items']
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
 
     products = Product.objects.all()
     context = {'products':products, 'cartItems': cartItems}
@@ -35,44 +36,10 @@ def cart(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
-        try:
-            cart = json.loads(request.COOKIES['cart'])
-        except:
-            cart = {}
-        
-        print('Cart:', cart)
-        #creating empty cart for non-logged users
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
-        cartItems = order['get_cart_items']
-
-        for i in cart:
-            #we use try block to prevent items in cart that may have been removed from admin
-            try:
-                cartItems += cart[i]["quantity"]
-
-                product = Product.objects.get(id=i)
-                total = (product.price * cart[i]["quantity"])
-
-                order['get_cart_total'] += total
-                order['get_cart_items'] += cart[i]['quantity']
-
-                item = {
-                    'product':{
-                        'id': product.id,
-                        'name': product.name,
-                        'price': product.price,
-                        'imageURL': product.imageURL,
-                        },
-                    'quantity': cart[i]['quantity'],
-                    'get_total': total
-                    }
-                items.append(item)
-
-                if product.digital == False:
-                    order['shipping'] = True
-            except:
-                pass
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
 
     context = {'items':items, 'order':order, 'cartItems': cartItems}
     return render(request, 'cart.html', context)
@@ -88,10 +55,10 @@ def checkout(request):
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
-        
-        items = []
-        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
-        cartItems = order['get_cart_items']
+        cookieData = cookieCart(request)
+        cartItems = cookieData['cartItems']
+        order = cookieData['order']
+        items = cookieData['items']
 
     context = {'items':items, 'order':order, 'cartItems': cartItems}
     return render(request, 'checkout.html', context)
